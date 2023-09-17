37ece583dc80b337864a97196d4994a9b64a85ce
Benjamin <mokhtarpour@engineer.com>
2023-09-17 19:26:30 +0330

    Initial Commit

---
+++ Mobilet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobilet
+//
+//  Created by Benyamin Mokhtarpour on 9/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MobiletUITests/MobiletUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MobiletUITestsLaunchTests.swift
+//  MobiletUITests
+//
+//  Created by Benyamin Mokhtarpour on 9/17/23.
+//
+
+import XCTest
+
+final class MobiletUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
